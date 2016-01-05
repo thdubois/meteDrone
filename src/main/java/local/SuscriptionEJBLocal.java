8705f49e4a5d86b40108d3124b5f38c8be39758a
JeremyBck <jeremy.bancker@gmail.com>
2016-01-05 14:25:21 +0100

    getSensorsUser done

--- src/main/java/local/SuscriptionEJBLocal.java
+++ src/main/java/local/SuscriptionEJBLocal.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 import javax.ejb.Local;
 
+import entity.Sensor;
 import entity.Suscription;
 
 @Local
 public interface SuscriptionEJBLocal {
 	public List<Suscription> findSuscriptionById(Long id);
 	public List<Suscription> findSuscriptions();
+	public List<Sensor> findSensorsUserById(Long id);
 	public void createSuscription(Date begin, Long idUser, Long idSensor);
 
 }
