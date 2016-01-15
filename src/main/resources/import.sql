6fcbcc85301694dd9b1270ed55394eec3043fb31
Thomas Dubois <thomasdubois000@gmail.com>
2016-01-15 11:44:26 +0100

    bug resolu logout

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,10 +3,10 @@ insert into drone(id, name, model, status, city) values (200, 'beta', 'KPC124', 
 insert into drone(id, name, model, status, city) values (300, 'gamma', 'KPC124', 'Fonctionnel', 'Montélimar')
 insert into drone(id, name, model, status, city) values (400, 'omega', 'KPC124', 'Fonctionnel', 'Pierrelatte')
 
-INSERT INTO SENSOR ( ID  , MODEL , NAME , PRICE, DRONE_ID  ) VALUES ( 200, 'OLKP986', 'Pressure Sensor', 12, 100)
-INSERT INTO SENSOR ( ID  , MODEL , NAME , PRICE, DRONE_ID  ) VALUES ( 300, 'ZER435', 'Altimeter', 12, 200)
-INSERT INTO SENSOR ( ID  , MODEL , NAME , PRICE, DRONE_ID  ) VALUES ( 400, 'UHI789', 'Light Sensor', 12, 300)
-INSERT INTO SENSOR ( ID  , MODEL , NAME , PRICE, DRONE_ID  ) VALUES ( 500, 'NJK043', 'Temperature Sensor', 12, 400)
+INSERT INTO SENSOR ( ID  , MODEL , NAME , PRICE, DRONE_ID  ) VALUES ( 200, 'OLKP986', 'Pressure Sensor', 45, 100)
+INSERT INTO SENSOR ( ID  , MODEL , NAME , PRICE, DRONE_ID  ) VALUES ( 300, 'ZER435', 'Altimeter', 87, 200)
+INSERT INTO SENSOR ( ID  , MODEL , NAME , PRICE, DRONE_ID  ) VALUES ( 400, 'UHI789', 'Light Sensor', 56, 300)
+INSERT INTO SENSOR ( ID  , MODEL , NAME , PRICE, DRONE_ID  ) VALUES ( 500, 'NJK043', 'Temperature Sensor', 66, 400)
 
 
 INSERT INTO analogic(id) VALUES (200)
